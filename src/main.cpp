7b9888ea0ba4689f97df2563ad22d920351fa11f
Neko_chan <mushbeen25@gmail.com>
2024-08-07 18:18:40 +0900

    P_Reg

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,27 @@
 
 BufferedSerial pc(USBTX, USBRX,115200);
 USonic usonic(PA_7, PA_6);
+CAN can(PA_11, PA_12, (int)1e6);
+CANMessage msg;
+int16_t output[4] = {0};
+int gain = 600;
 
 
 int main() {
+    int goal = 5;
 
     while(1) {
+        int pos;
         int cm = usonic.distance();
-        if (cm)
-        printf("distance: %d cm\n", cm);
+        if (cm){
+        pos = cm;
+        
+}
+
+        
+        output[0] = (pos - goal) * gain;
+        printf("distance: %d cm, output: %d\n", pos,output[0]);
+        CANMessage msg(4, (const uint8_t *)output, 8); //メッセージ構築
+        can.write(msg); //CAN送信
     }
 }
