86e88a227513f9c4bbfbb40c008683ba4c06000c
Neko_chan <mushbeen25@gmail.com>
2024-08-08 09:18:25 +0900

    PD

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,23 @@ USonic usonic(PA_7, PA_6);
 CAN can(PA_11, PA_12, (int)1e6);
 CANMessage msg;
 int16_t output[4] = {0};
-int gain = 600;
+int gain[2] = {600, 100};
 
 
 int main() {
     int goal = 5;
+    int pre_error = 0;
 
     while(1) {
         int pos;
         int cm = usonic.distance();
         if (cm){
         pos = cm;
-        
-}
+        }
+        int error = (pos - goal);
+        int deriv = error - pre_error;
+        output[0] = error * gain[0] + deriv * gain[1] + 1500; 
 
-        
-        output[0] = (pos - goal) * gain;
         printf("distance: %d cm, output: %d\n", pos,output[0]);
         CANMessage msg(4, (const uint8_t *)output, 8); //メッセージ構築
         can.write(msg); //CAN送信
