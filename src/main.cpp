f817273638dc6b9d69ac8a144d25ff4491d1911a
Neko_chan <mushbeen25@gmail.com>
2024-08-08 13:40:15 +0900

    Clamp add

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
 #include "mbed.h"
 #include "usonic.hpp"
 
+
 BufferedSerial pc(USBTX, USBRX,115200);
 USonic usonic(PA_7, PA_6);
 CAN can(PA_11, PA_12, (int)1e6);
 CANMessage msg;
 int16_t output[4] = {0};
-int gain[2] = {600, 100};
+float gain[3] = {700, 0.01, 100};
+
+int clamp(int value, int min, int max) {
+    if (value < min) {
+        return min;
+    } else if (value > max) {
+        return max;
+    } else {
+        return value;
+    }
+}
 
 
 int main() {
     int goal = 5;
     int pre_error = 0;
+    int integral = 0;
 
     while(1) {
         int pos;
@@ -20,8 +32,11 @@ int main() {
         pos = cm;
         }
         int error = (pos - goal);
+        integral += error;
         int deriv = error - pre_error;
-        output[0] = error * gain[0] + deriv * gain[1] + 1500; 
+        output[0] = error * gain[0] + integral * gain[1] + deriv * gain[2];
+        output[0] = clamp(output[0], -13000, 13000);
+        pre_error = error;
 
         printf("distance: %d cm, output: %d\n", pos,output[0]);
         CANMessage msg(4, (const uint8_t *)output, 8); //メッセージ構築
